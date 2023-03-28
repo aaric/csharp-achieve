c5dfa4c6124ca768d13a3bca0111833d6d62f2bc
qlhan <qlhan@incarcloud.com>
2023-03-28 14:18:57 +0800

    chore: auto init sample
    hello world

---
+++ csharp-achieve/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_achieve
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("hello world");
+            Console.ReadKey();
+        }
+    }
+}
