b7b53fc7dcca275581b72494d8cdeb91b9a38e00
qlhan <qlhan@incarcloud.com>
2023-04-06 18:02:20 +0800

    feat: xml file lang
    create ok

--- ConsoleLang/Lang/FilePrinter.cs
+++ ConsoleLang/Lang/FilePrinter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Xml;
 
 namespace ConsoleLang.Lang
 {
@@ -8,15 +9,56 @@ namespace ConsoleLang.Lang
     {
         public void print()
         {
+            // txt
+            //loadTxtFile();
+
+            // xml
+            loadXmlFile();
+        }
+
+        private void loadXmlFile()
+        {
+            string path = @"E:\test_file\mytext.xml";
+
+            // 创建XML文件
+            XmlDocument document = new XmlDocument();
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", Encoding.UTF8.BodyName, "");
+            document.AppendChild(declaration);
+
+            XmlElement user = document.CreateElement("Student");
+            user.SetAttribute("Type", "University");
+
+            XmlElement userId = document.CreateElement("Id");
+            userId.InnerText = "1";
+            XmlElement userName = document.CreateElement("Name");
+            userName.InnerText = "Aaric";
+            XmlElement userAge = document.CreateElement("Age");
+            userAge.InnerText = "18";
+
+            user.AppendChild(userId);
+            user.AppendChild(userName);
+            user.AppendChild(userAge);
+            document.AppendChild(user);
+
+            document.Save(path);
+            
+            // 读取XML文件
+            document = new XmlDocument();
+            document.Load(path);
+            XmlElement root = document.DocumentElement;
+            foreach (XmlNode node in root.ChildNodes)
+            {
+                Console.WriteLine(node["Id"].InnerText);
+            }
+        }
+
+        private void loadTxtFile()
+        {
             string path = @"E:\test_file\mytext.txt";
 
             try
             {
-                if (File.Exists(path))
-                {
-                    File.Delete(path);
-                }
-
+                // 创建文本文件
                 using (StreamWriter sw = new StreamWriter(path))
                 {
                     sw.WriteLine("hello world");
@@ -26,9 +68,11 @@ namespace ConsoleLang.Lang
                     sw.WriteLine("hello csharp");
                 }
 
+                // 读取文本文件
                 string content = File.ReadAllText(path, Encoding.UTF8);
                 Console.WriteLine($"{content}");
 
+                // 读取文本文件
                 int index = 0;
                 using (StreamReader sr = new StreamReader(path))
                 {
@@ -37,6 +81,11 @@ namespace ConsoleLang.Lang
                         index++;
                         Console.WriteLine($"{index}: {sr.ReadLine()}");
                     }
+                }
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
                 }
             }
             catch (Exception e)
